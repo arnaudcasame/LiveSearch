073f8784b418992d7b4932b5c58ba39875c251e8
Arnaud C <djcase001@gmail.com>
2016-11-18 13:19:18 -0500

    Adds the javascript transpiling task

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,10 @@ gulp.task('html', function(){
     gulp.src('builds/development/**/*.html');
 });
 
+gulp.task('js', function(){
+    gulp.src('builds/development/js/**/*.js');
+});
+
 gulp.task('sass', function () {
     return sass('process/sass/style.scss', {
         sourcemap: true,
@@ -21,6 +25,7 @@ gulp.task('sass', function () {
 
 gulp.task('watch', function() {
     gulp.watch('builds/development/**/*.html', ['html']);
+    gulp.watch('builds/development/js/**/*.js', ['js']);
     gulp.watch(['process/sass/**/*'], ['sass']);
 });
 
